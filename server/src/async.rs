a27e44b9dea666374c03e9aea3765ffbe7dbfa13
Adolfo Ochagavía <aochagavia92@gmail.com>
2017-07-16 15:41:36 +0200

    More async code cleanup

--- server/src/async.rs
+++ server/src/async.rs
@@ -1,4 +1,5 @@
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
 use std::rc::Rc;
 
 use futures::{future, sink, stream, AsyncSink, Future, Sink, Stream};
@@ -8,14 +9,7 @@ use tokio_io::{codec, AsyncRead};
 
 use protocol::async::ChatCodec;
 
-type ActiveClients = Rc<RefCell<Vec<Client>>>;
 type MsgSink = sink::Buffer<stream::SplitSink<codec::Framed<TcpStream, ChatCodec>>>;
-
-struct Client {
-    id: u32,
-    nickname: Option<String>,
-    sink: MsgSink,
-}
 
 pub fn run() {
     let mut core = Core::new().expect("Unable to initialize tokio core");
@@ -24,92 +18,95 @@ pub fn run() {
 }
 
 pub fn start_server(handle: &Handle) {
-    // The client information corresponding to all open connections will be kept in this vector
-    let active_clients = Rc::new(RefCell::new(Vec::new()));
+    let broadcaster = Rc::new(Broadcaster::default());
 
     // Setup the listener
     let addr = "127.0.0.1:8080".parse().expect("Unable to parse socket address");
     let listener = TcpListener::bind(&addr, handle).expect("Unable to launch tcp listener").incoming();
 
     // Accept new connections and specify how incoming messages will be handled
-    let client_ids = stream::iter((0..).map(Ok));
     let owned_handle = handle.clone();
-    handle.spawn(client_ids.zip(listener).for_each(move |(client_id, tcp_stream)| {
-        println!("[debug] new client");
+    handle.spawn(listener.for_each(move |(tcp_stream, _)| {
+        let (msg_sink, msg_stream) = tcp_stream.framed(ChatCodec).split();
+        let client_id = broadcaster.subscribe(msg_sink.buffer(100));
 
-        let (msg_sink, msg_stream) = tcp_stream.0.framed(ChatCodec).split();
-        let msg_sink = msg_sink.buffer(100);
-
-        // Track active clients
-        active_clients.borrow_mut().push(Client {
-            id: client_id,
-            nickname: None,
-            sink: msg_sink
-        });
+        println!("[debug] new connection ({})", client_id);
 
         // Setup broadcasting of incoming messages from this client to all other active clients
-        let active_clients = active_clients.clone();
-        owned_handle.spawn(msg_stream.for_each(move |msg| {
-            broadcast_message(&active_clients, client_id, msg);
-            future::ok(())
-        }).map_err(|_| println!("[debug] connection closed")));
+        let broadcaster = broadcaster.clone();
+        owned_handle.spawn(msg_stream.into_future().and_then(move |(opt_nickname, msg_stream)| {
+            // The first message is the nickname
+            let nickname = opt_nickname.unwrap_or(String::new());
+            broadcaster.broadcast(client_id, format!("{} has logged in", nickname));
+
+            // All further messages can just be forwarded
+            let broadcaster = broadcaster.clone();
+            msg_stream.for_each(move |msg| {
+                let msg = format!("{}: {}", nickname, msg);
+                broadcaster.broadcast(client_id, msg);
+                future::ok(())
+            }).map_err(move |_| {
+                println!("[debug] stream closed ({})", client_id);
+
+                // FIXME: this panic should be removed, but the type system doesn't like it...
+                panic!()
+            })
+        }).map_err(|_| panic!("FIXME: What should we do?")));
 
         future::ok(())
     }).map_err(|e| panic!("Error listening to TCP connections: {}", e)));
 }
 
-/// Broadcast a message to all active clients
-fn broadcast_message(active_clients: &ActiveClients, client_id: u32, msg: String) {
-    let mut active_clients = active_clients.borrow_mut();
+#[derive(Default)]
+struct Broadcaster {
+    next_client_id: Cell<usize>,
+    subscribers: RefCell<HashMap<usize, MsgSink>>
+}
 
-    // Get the nickname of the client
-    // If the nickname is None, it means that this message is the nickname!
-    let message = {
-        let nickname = &mut active_clients.iter_mut().find(|c| c.id == client_id).expect("Message from inactive client?").nickname;
-        match nickname.clone() {
-            Some(n) => format!("{}: {}", n, msg),
-            None => {
-                *nickname = Some(msg.clone());
-                format!("{} has logged in", msg)
-            }
-        }
-    };
+impl Broadcaster {
+    fn subscribe(&self, sink: MsgSink) -> usize {
+        // Generate a new id
+        let client_id = self.next_client_id.get();
+        self.next_client_id.set(client_id + 1);
 
-    // Prepare the message to be sent
-    println!("[message] {}", message);
-
-    // When a send fails, we will store the index of the client in this
-    // vector. This way we can remove clients that have disconnected
-    let mut disconnected = Vec::new();
-
-    for (i, client) in active_clients.iter_mut().enumerate() {
-        // Don't send messages to clients that haven't provided a nickname
-        if client.nickname.is_none() {
-            continue;
-        }
-
-        // Don't send messages to yourself
-        if client.id == client_id {
-            continue;
-        }
-
-        match client.sink.start_send(message.clone()).expect("unexpected error") {
-            AsyncSink::Ready => (),
-            AsyncSink::NotReady(_) => println!("[debug] sink full, dropping message: {}", message)
-        }
-
-        // We need `poll_complete` to flush the sink
-        // FIXME: is this correct? This is the code I am least confident with in this crate
-        if let Err(_) = client.sink.poll_complete() {
-            println!("[debug] connection closed");
-            disconnected.push(i);
-        }
+        // Subscribe to broadcasts
+        self.subscribers.borrow_mut().insert(client_id, sink);
+        client_id
     }
 
-    // Remove closed connections from our active client list
-    let mut deleted_count = 0;
-    for i in disconnected {
-        active_clients.swap_remove(i - deleted_count);
-        deleted_count += 1;
+    fn broadcast(&self, ignore_id: usize, msg: String) {
+        let mut subscribers = self.subscribers.borrow_mut();
+
+        // Prepare the message to be sent
+        println!("[message] {}", msg);
+
+        // When a send fails, we will store the index of the client in this
+        // vector. This way we can remove clients that have disconnected
+        let mut closed_connections = Vec::new();
+
+        for (&client_id, client) in subscribers.iter_mut() {
+            // Don't send messages to yourself
+            if client_id == ignore_id {
+                continue;
+            }
+
+            match client.start_send(msg.clone()).expect("unexpected error") {
+                AsyncSink::Ready => (),
+                AsyncSink::NotReady(_) => println!("[debug] sink full, dropping message: {}", msg)
+            }
+
+            // We need `poll_complete` to flush the sink
+            // FIXME: is this correct? This is the code I am least confident with in this crate
+            if let Err(_) = client.poll_complete() {
+                println!("[debug] connection closed");
+                closed_connections.push(client_id);
+            }
+        }
+
+        // Remove connections that are closed so we don't broadcast to them in the future
+        for id in closed_connections {
+            println!("[debug] broadcast failed, removing socket from broadcaster");
+            subscribers.remove(&id);
+        }
     }
 }
